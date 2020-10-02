9965a6532f4551d94518817e542a85af84504f77
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-02 12:21:47 +0900

    ページ遷移

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+
+
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+  res.render('top.ejs');
 });
 
-app.get('/top', (req, res) => {
-  res.render('top.ejs');
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
 });
 
 app.listen(3000);
