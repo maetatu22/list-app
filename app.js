c61db59b480881ff01eb13124668f50918c10258
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-02 12:21:47 +0900

    mysql

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const express = require('express');
-
 const app = express();
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'list_app'  
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
 
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+      res.render('top.ejs');
 });
 
-app.get('/top', (req, res) => {
-  res.render('top.ejs');
+
+
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      console.log(results);
+      res.render('index.ejs');
+    }
+  );
+  
 });
 
-app.listen(3000);
+
 
 app.use(express.static('public'));
+
+app.listen(3000);
