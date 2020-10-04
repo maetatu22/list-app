7b4d50b3182d6512e0680d009a6176307c2ed8cc
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-04 23:05:11 +0900

    メモ保存をデータベースに保存して表示させる

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+app.use(express.urlencoded({extended: false})); //フォームから送信された値を受け取れるようにする定形文
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '', //いざとなったらパスワードは空でオッケー
+  password: "", //いざとなったらパスワードは空でオッケー
   database: 'list_app'  
 });
 
@@ -37,10 +38,27 @@ app.get('/index', (req, res) => {
 
 app.get('/new', (req,res) => {
   res.render('new.ejs');
-})
+});
+
+app.post('/create', (req, res) => {
+  connection.query(
+    'INSERT INTO item(name)VALUES(?)',
+    [req.body.itemName],
+    (error,results) => {
+      connection.query(
+        'SELECT * FROM items',
+        (error,results) => {
+          res.render('index.ejs', {items: results});
+        }
+      );
+    }
+  );
+  
+});
 
 
 
 app.use(express.static('public'));
+
 
 app.listen(3000);
